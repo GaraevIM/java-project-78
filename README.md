e9c178b22143324c9496506d1b69896aaf8b9d8f
Ильдар <https://github.com/araev/java-project-61>
2026-03-02 20:50:12 +0300

    chore: setup CI, SonarCloud and project structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-### Hexlet tests and linter status:
-[![Actions Status](https://github.com/GaraevIM/java-project-78/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/GaraevIM/java-project-78/actions)
+[![CI](https://github.com/GaraevIM/java-project-78/actions/workflows/gradle.yml/badge.svg?branch=main)](https://github.com/GaraevIM/java-project-78/actions/workflows/gradle.yml)
+[![hexlet-check](https://github.com/GaraevIM/java-project-78/actions/workflows/hexlet-check.yml/badge.svg?branch=main)](https://github.com/GaraevIM/java-project-78/actions/workflows/hexlet-check.yml)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=GaraevIM_java-project-78&metric=alert_status)](https://sonarcloud.io/project/overview?id=GaraevIM_java-project-78)
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=GaraevIM_java-project-78&metric=coverage)](https://sonarcloud.io/project/overview?id=GaraevIM_java-project-78)
---
+++ app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    jacoco
+    id("org.sonarqube") version "6.0.1.5171"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "GaraevIM_java-project-78")
+        property("sonar.organization", "garaevim")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+    }
+}
